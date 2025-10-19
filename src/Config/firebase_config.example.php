0f839f6447b5e7b57eab3d82ac96e171b792f80a
Onice-04 <mendozaonice25@gmail.com>
2025-10-19 01:36:24 -0600

    modificaciones

---
+++ src/Config/firebase_config.example.php
@@ -0,0 +1,11 @@
+<?php
+
+/**
+ * Configuración de Firebase - ARCHIVO DE EJEMPLO
+ * Copia este archivo como firebase_config.php y agrega tus claves reales
+ */
+
+return [
+    'project_id' => 'tu_project_id_aqui',
+    'web_api_key' => 'tu_web_api_key_aqui',
+];
