a7c1a4ec00be18a5bbed50b940a248ce69ae5238
blessmevid <emelyvidea14@gmail.com>
2025-10-26 02:06:41 +0200

    .

--- src/Config/responseHTTP.php
+++ src/Config/responseHTTP.php
@@ -4,12 +4,17 @@
 namespace App\Config; //este sera nuestro espacio de nombres
 
 class responseHTTP{
-    //definimos una variable llamada mensaje que sera un array asociativo para nuestros mensajes
     public static $mensaje = array(
         'status' => '',
         'message' => '',
-        'data' => ''
+        'data' => null
     );
+
+    public static function sendResponse($data) {
+        header('Content-Type: application/json');
+        echo json_encode($data);
+        exit;
+    }
 
     //creamos nuestro primer codigo de estado http
 
@@ -43,7 +48,7 @@ class responseHTTP{
         return self::$mensaje;
     }
 
-    final public static function status404(){
+    final public static function status404($res = 'No existe el recurso solicitado!'){
         $res = 'No existe  el recurso solicitado!';
         http_response_code(404);
         self::$mensaje['status'] = 'ERROR';
@@ -51,7 +56,7 @@ class responseHTTP{
         return self::$mensaje;
     }
 
-    final public static function status500(){
+    final public static function status500($res = 'Se ha producido un error en el servidor!'){
         $res = 'Se ha producido un error en el servidor!';
         http_response_code(500);
         self::$mensaje['status'] = 'ERROR';
